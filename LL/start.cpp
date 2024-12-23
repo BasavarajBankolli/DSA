f6d26f8cf77125bc5ce771c049385e3c9ec8b647
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-23 19:38:41 +0530

    Update start.cpp

--- LL/start.cpp
+++ LL/start.cpp
@@ -1,1 +1,28 @@
+```cpp
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public: 
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main(){
+    vector <int> vec = {1,2,3,4};
+    Node x = Node(2); // here x is variable and Node is datatype stored for that
+    Node* y = &x;      // memory address of that x 
+
+    cout << y->data;
+}
+  
+
+```
